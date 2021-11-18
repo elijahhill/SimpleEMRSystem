51b62ed689ff03b94bc881f10d06a0f0261d9283
Elijah Hill <elijahhill@u.boisestate.edu>
2021-11-18 01:11:25 -0700

    WIP - Checking in observations to compare previous translation file

--- data_type_translation/translator.py
+++ data_type_translation/translator.py
@@ -1,3 +1,4 @@
+from typing import Dict
 from numpy.core.numeric import NaN
 import pandas as pd
 import json
@@ -6,6 +7,7 @@ from pandas.core.frame import DataFrame
 from os import path
 import PySimpleGUI as sg
 
+
 class PathContainer:
     def __init__(self):
         pass
@@ -13,70 +15,68 @@ class PathContainer:
     def add_excel_path(self, excel_path: str):
         self.excel_path = excel_path
 
-    def add_output_folder(self, output_folder: str):
-        self.output_folder = output_folder
+    def add_output_path(self, output_path: str):
+        self.output_path = output_path
 
     def get_excel_path(self):
         return self.excel_path
 
-    def get_output_folder(self):
-        return self.output_folder
+    def get_output_path(self):
+        return self.output_path
 
-    
+
 class StudyCreator:
-
-    def __read_data(self):
-
-        data_paths = self.__get_data_path()
-
-        df = pd.read_excel(data_paths.get_excel_path(), engine="openpyxl")
-
-        df["ecart percentile"] = df["ecart percentile"].round(1)
-
-        self.df = df
-    
-        translation = None
-        with open("./data_type_translation/stat_lookup.json") as f:
-            translation = json.load(f)
-
-        if(translation != None):
-            self.keys = translation.keys()
-        else:
-            raise Exception("Translation file is empty")
-
-    def __get_data_path(self) -> PathContainer:
-        dirname = path.dirname(__file__)
-        json_paths = path.join(dirname, "./paths.json")
+    def __get_data_paths(self, current_path: str) -> PathContainer:
+        json_paths = path.join(current_path, "./paths.json")
         with open(json_paths) as paths_fp:
             paths = json.load(paths_fp)
 
         path_container = PathContainer()
-        
+
         layout = [[sg.T("")], [sg.Text("Choose the excel data file: "), sg.Input(default_text=paths["excel_path"]),
-                            sg.FileBrowse(key="-ExcelFile-")], 
-                  [sg.T("")], [sg.Text("Choose the output folder "), sg.Input(default_text=paths["output_path"]),
-                               sg.FileBrowse(key="-OutputFolder-")], [sg.Button("Submit")]]
+                               sg.FileBrowse(key="-ExcelFile-")],
+                  [sg.T("")], [sg.Text("Choose the output path (as a folder) "), sg.Input(default_text=paths["output_path"]),
+                               sg.FolderBrowse(key="-OutputFolder-")],
+                  [sg.Button("Submit")]]
         window = sg.Window('Locate File', layout)
-        
+
         while True:
             event, values = window.read()
             if event == sg.WIN_CLOSED or event == "Exit":
                 break
             elif event == "Submit":
                 # TODO: Need to add checks to determine file validity
-                path_container.add_excel_path(values["-ExcelFile-"])
-                path_container.add_output_folder(values["-OutputFolder-"])
+
+                # Using indexes because the keys don't take into account pre-entered file paths
+                path_container.add_excel_path(values[0])
+                path_container.add_output_path(values[1])
 
                 paths["excel_path"] = path_container.get_excel_path()
-                paths["output_path"] = path_container.get_output_folder()
+                paths["output_path"] = path_container.get_output_path()
                 with open(json_paths, "w") as paths_fp:
-                    json.dump(paths, paths_fp)
+                    json.dump(obj=paths, fp=paths_fp, indent=4)
 
                 window.close()
 
         return path_container
-    
-    def __create_user_details(self, user_names: list, patients: list):
+
+    def __get_df(self, excel_path: str):
+        df = pd.read_excel(excel_path, engine="openpyxl")
+        df["ecart percentile"] = df["ecart percentile"].round(1)
+        return df
+
+    def __get_translation(self, current_path: str) -> dict:
+        translation = None
+        with open(f"{current_path}/stat_lookup.json") as f:
+            translation = json.load(f)
+
+        if(translation != None):
+            return translation
+        else:
+            raise Exception("Translation file is empty")
+
+    def __create_user_details(self, user_names: list, patients: list) -> Dict:
+        print("creating user details")
         all_user_details = {}
         for user in user_names:
             user_details = {
@@ -87,7 +87,8 @@ class StudyCreator:
             all_user_details[user] = user_details
         return all_user_details
 
-    def __create_case_details(self, case_ids: set, min_time: float=1352687400000.0, max_time: float=135294660000.0):
+    def __create_case_details(self, case_ids: set, min_time: float = 1352687400000.0, max_time: float = 135294660000.0) -> Dict:
+        print("Creating case details")
         all_case_details = {}
         for case_id in case_ids:
             details_list = [
@@ -96,7 +97,7 @@ class StudyCreator:
                     "max_t": max_time,
                     "check_boxes": 0,
                     "instruction_set": "familiar"
-                }, 
+                },
                 {
                     "min_t": min_time,
                     "max_t": max_time,
@@ -107,18 +108,57 @@ class StudyCreator:
             all_case_details[case_id] = details_list
         return all_case_details
 
-    def create_study(self):
-        self.__read_data()
+    def __create_data_layout(self):
+        print("Creating data layout")
+        return {
+            "title_bar": ["id", "age", "sex", "height", "weight", "bmi", "race"],
+            "risk_score_and_vitals": ["ECART", "HR", "RR", "BP", "SaO2", "Temperature"],
+            "neurology": ["AVPU"],
+            "blood_gas_cbc_lactate": ["pC02", "HgB", "WBC", "Platelets", "Lactate"],
+            "chemistry": ["Sodium", "Potassium", "Chloride", "Anion_Gap", "Gluc_Ser", "Bun", "Bun_Cr_Ratio", "Calcium"]
+        }
 
-        patients = set(self.df["patient_id"])
-        first_patient = [patients.pop()]
+    def create_study(self):
+        print("Fetching data")
+        current_path = path.dirname(__file__)
+        data_paths = self.__get_data_paths(current_path=current_path)
+        excel_path = data_paths.get_excel_path()
+        output_folder_path = data_paths.get_output_path()
+
+        df = self.__get_df(excel_path=excel_path)
+        translation = self.__get_translation(current_path=current_path)
+
+        # patients = set(df["patient_id"])
+
+        # Make sure all of these elements are strings
+        # first_patient = [str(patients.pop())]
+
+        case_ids = set(df["patient_id"])
+
+        case_ids_str_list = list(map(lambda elem: str(elem), case_ids))
 
         users = ["testuser1"]
-        user_details = self.__create_user_details(user_names=users, patients=first_patient)
+        user_details = self.__create_user_details(
+            user_names=users, patients=case_ids_str_list)
+        with open(f"{output_folder_path}/user_details.json", "w+") as fp:
+            json.dump(obj=user_details['testuser1'], fp=fp, indent=4)
 
-        case_ids = set(self.df["patient_id"])
+        case_ids = set(df["patient_id"])
         case_details = self.__create_case_details(case_ids=case_ids)
-        return case_details
+        with open(f"{output_folder_path}/case_details.json", "w+") as fp:
+            json.dump(obj=case_details, fp=fp, indent=4)
+
+        print("Creating data layout")
+        data_layout = self.__create_data_layout()
+        with open(f"{output_folder_path}/data_layout.json", "w+") as fp:
+            json.dump(obj=data_layout, fp=fp, indent=4)
+
+        print("Creating variable details")
+        with open(f"{current_path}/variable_details.json") as fp:
+            variable_details = json.load(fp=fp)
+
+        with open(f"{output_folder_path}/variable_details.json", "w+") as fp:
+            json.dump(obj=variable_details, fp=fp, indent=4)
 
 
 
@@ -126,7 +166,8 @@ user_index = 0
 user_name = f"user{user_index}"
 creator = StudyCreator()
 # print(creator.create_study([user_name]))
-print(creator.create_study())
+# print(creator.create_study())
+creator.create_study()
 
 
 # def add_to_start_time(self, hours: float):
@@ -149,7 +190,6 @@ print(creator.create_study())
 #     new_data = hours_and_hr_no_nan.values.tolist()
 
 #     return new_data
-
 
 
 #     one_patient = df[df["patient_id"] == 610044]
