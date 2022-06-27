49a08ff19a6e96e9f67e1fd6034a9938068ce680
elijahhill <elijahhill@u.boisestate.edu>
2022-06-27 16:30:08 -0600

    Changing LEMR to SEMR

--- SEMRproject/wsgi.py
+++ SEMRproject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for LEMRProject project.
+WSGI config for SEMRProject project.
 
 This module contains the WSGI application used by Django's development server
 and any production WSGI deployments. It should expose a module-level variable
@@ -15,7 +15,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LEMRProject.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SEMRproject.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
