4ad5ef6231fbfdcf858bedf0b7bfaa5dbe01602d
elijahhill <elijahhill@u.boisestate.edu>
2022-06-27 16:13:00 -0600

    Correcting interface to project

--- config/dev.py
+++ config/dev.py
@@ -1,7 +1,7 @@
 """Gunicorn *development* config file"""
 
 # Django WSGI application path in pattern MODULE_NAME:VARIABLE_NAME
-wsgi_app = "SEMRinterface.wsgi:application"
+wsgi_app = "SEMRproject.wsgi:application"
 # The granularity of Error log outputs
 loglevel = "debug"
 # The number of worker processes for handling requests
